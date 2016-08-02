21ec955b8277cc815cc436d36910ae9705704b09
Denis <deschwarz@gmx.de>
2016-08-02 16:20:20 +0200

    Zwischenstand

--- layer.js
+++ layer.js
@@ -12,54 +12,58 @@ $(function() {
 
         function Layer() {
 
-            // console.log(this)
-
             var globalName = 'layer';
 
             var config = {
                 initClass: 'js-init-' + globalName, //inits function of layer
-                // overlayClass: globalName + '-overlay', //needs for fixed overlay
                 wrapperClass: globalName + '-wrapper', //needs for toggling show/hide)
-                contentClass: globalName + '-content' //needs for styling
+                indexContentClass: globalName + '-index', //needs for Basic-CSS
+                contentClass: globalName + '-content', //place html-content of Layer here
+                contentOrigin: globalName + '-origin',
+                closeElement: globalName + '-close',
+                closeElementText: 'X',
+                isScrolling: globalName + '-scrolling'
             };
 
-            //get layer id
-            // var layerId = $('.' + config.initClass).attr('id');
+            var options = {
 
-            //get initial name
-            // var layerDataName = $('body').find('*[data-' + globalName +']').data(globalName);
-
-            var defaultOptions = {
-                //@TODO initial Werte finden
-                width: 800,
-                height: 600,
-                margin: 10,
-                background: '???'
-            };
-
-            var init = function() {
-                console.log('inited')
-                defineLayerElements(); 
-                onClickLink();
-                $('.' + config.initClass).show();
-            };
-
-            var defineLayerElements = function() {
-                var wrapperElement = markup('div', config.wrapperClass);
-                    // overlayElement = markup('div', config.overlayClass);
+                styleClass: '',
+                close: 'button', //button, overlay
+                
+                customStyle: {
+                    padding: '',
+                    backgroundOverlay: '',
+                    border: ''
+                }
             };
 
             /**
              * 
              */
-            var markup = function(sElement, sClassName) {
-                var oElement = document.createElement(sElement), // build div
+            var init = function() {
+                buildElement(); 
+                openLayer();
+                closeLayer();
+            };
+
+            /**
+             * 
+             */
+            var buildElement = function() {
+                buildMarkup('div', config.wrapperClass);
+                buildMarkup('div', config.closeElement);
+            };
+
+            /**
+             * position wrapper to start of body 
+             * and other builded element as child of wrapper
+             */
+            var buildMarkup = function(sElement, sClassName) {
+                var oElement = document.createElement(sElement),
                     sParent = '.' + config.wrapperClass;  
 
-                oElement.className = sClassName;    // name  class
+                oElement.className = sClassName; // name class
 
-                //position wrapper to start of body 
-                // and other builded element as child of wrapper
                 if ($(oElement).hasClass(config.wrapperClass)) {
                     sParent = 'body';
                 }
@@ -69,26 +73,61 @@ $(function() {
             /**
              * 
              */
-            var onClickLink = function() {
+            var openLayer = function() {
                 $('body').find('*[data-' + globalName +']').on('click', function(e) {
-                    console.log('sdfljk')
                     e.preventDefault();
-                    var sDataId = $(this).data(globalName); // get layer id
+                    var sDataId = $(this).data(globalName), // get layer id
+                        sDataIdSelektor = '#' + sDataId,
+                        sIdentifyClass = globalName + '-' + sDataId; // flag opened Layer (get class from layer id)
+                        
+                    //check layer has opened before
+                    if (!$('.' + config.wrapperClass).hasClass(sIdentifyClass)) {
+                        
+                    var sInnerHtml =   '<div class=' + config.contentClass + '>'
+                                    +   '<div class=' + config.contentOrigin + '>' 
+                                    +      $(sDataIdSelektor).html()
+                                    +   '</div>' 
+                                    + '</div>'
 
-                    //move layer content to layer wrapper and add content class
-                    $('#' + sDataId).addClass(config.contentClass).appendTo('.' + config.wrapperClass);
+                        //wrap users layer-content into divs
+                        $(sDataIdSelektor).html(sInnerHtml);
 
-                    $('.' + config.wrapperClass).show();
+                        //move users layer-content to layer wrapper and add index-content class
+                        $(sDataIdSelektor).addClass(config.indexContentClass).appendTo('.' + config.wrapperClass);
 
+                        //move and place close-element and text this
+                        $('.' + config.closeElement).text(config.closeElementText).prependTo('.' + config.indexContentClass);
+                    }
+                    
+                    //show wrapper and this layer
+                    $('.' + config.wrapperClass).addClass(sIdentifyClass).add(sDataIdSelektor).show();
 
+                    hasScrollContent(sDataIdSelektor);
                 });
-                
 
             };
 
-            //is users input valid
-            // 
-            console.log()
+            /**
+             * 
+             */
+            var closeLayer = function() {
+                $('.' + config.closeElement).on('click', function() {
+                    $(this).parents('.' + config.indexContentClass).add('.' + config.wrapperClass).hide();
+                });
+            };
+
+            /**
+             * fires scroll class to avoid overflow
+             */
+            var hasScrollContent = function(elementId) {
+                if($(elementId).children('.' + config.contentClass).outerHeight(true) > $(elementId).outerHeight(true)) {
+                    $(elementId).addClass(config.isScrolling);
+                }
+            };
+
+            /**
+             * init if init class exists
+             */
             if (document.getElementsByClassName(config.initClass).length) {
                 init();
             } else {
